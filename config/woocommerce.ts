db23144f6f2bc2e46a3f45f6ed3c23c5ab2fbf6c
admondtamang <admondtamang@gmail.com>
2022-02-28 21:53:48 +0545

    [add] shippr json

--- config/woocommerce.ts
+++ config/woocommerce.ts
@@ -1,6 +1,11 @@
-const consumerKey = 'ck_f46f554a752781da258a8f2664a2c91e517c2bf8';
-const consumerSecret = 'cs_6e09b626b18f72d74c4929700d98e2989e0e9b1a';
+const baseURL = 'http://35.154.133.111:81/';
+// const baseURL = "http://shippr.local/";
 
-const baseURL = 'https://shippr.store/';
+// const consumerKey = "ck_e4402ba377a6a237a2b01e5b3d9cd1492403112c";
+// const consumerSecret = "cs_6db346bff344e4548d3a28810e850b923e781b05";
+
+// sulav shopping
+const consumerKey = 'ck_d5afcef8dd078d78f83c6da8d8d9a36cb1a1a729';
+const consumerSecret = 'cs_b792b15b8306ffadcf972036e98340bc156502d0';
 
 export { consumerKey, consumerSecret, baseURL };
--- pages/api/feeder/index.ts
+++ pages/api/feeder/index.ts
@@ -11,23 +11,30 @@ type Data = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const data = req.body;
   switch (req.method) {
-    case 'GET':
-      const bucket: any = await JsonBucket.find({});
+    // case 'GET':
+    //   const bucket: any = await JsonBucket.find({});
 
-      res.status(200).json({ name: '', data: bucket });
+    //   res.status(200).json({ name: '', data: bucket });
 
-      break;
+    //   break;
     case 'POST':
-      const result: any = await Promise.all(
-        data.product_ids.map(async (product_id: Number) => await axiosInstance.get('wp-json/wc/v3/products/' + product_id))
-      );
-      //filter data.data
-      const finalResult = result.map((last: any) => last.data);
+      try {
+        const result: any = await Promise.all(
+          data.product_ids.map(async (product_id: Number) => await axiosInstance.get('wp-json/wc/v3/products/' + product_id))
+        );
 
-      res.status(200).json({
-        name: 'POST',
-        data: { name: data?.title, icon: data?.icon, category_id: data?.category_id, products: finalResult },
-      });
+        //filter data.data
+        const finalResult = result.map((last: any) => last.data);
+
+        res.status(200).json({
+          name: 'POST',
+          data: { name: data?.title, icon: data?.icon, category_id: data?.category_id, products: finalResult },
+        });
+      } catch (error) {
+        console.log('provide valid id');
+
+        console.log('Error', error);
+      }
 
       break;
 
